3dcb91de8da6913de905a3fea9406b2ec94d375a
Zhin10 <zlpalacios@hotmail.com>
2021-12-11 19:55:14 -0300

    Feature: Reportar Datos Recibido en Consola

---
+++ Código de Transmisor y Recepción/Recepción/Recepcion.ino
@@ -0,0 +1,55 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+ 
+//Declaremos los pines CE y el CSN
+#define CE_PIN 4
+#define CSN_PIN 5
+ 
+//Variable con la dirección del canal que se va a leer
+byte direccion[5] ={'c','a','n','a','l'};
+
+//creamos el objeto radio (NRF24L01)
+RF24 radio(CE_PIN, CSN_PIN);
+
+//vector para los datos recibidos
+float datos[3];
+
+void setup() {
+  //inicializamos el NRF24L01 
+  radio.begin();
+  //inicializamos el puerto serie
+  Serial.begin(9600); 
+  
+  //Abrimos el canal de Lectura
+  radio.openReadingPipe(1, direccion);
+  
+    //empezamos a escuchar por el canal
+  radio.startListening();
+
+}
+
+void loop() {
+ uint8_t numero_canal;
+ //if ( radio.available(&numero_canal) )
+ if ( radio.available() )
+ {    
+     //Leemos los datos y los guardamos en la variable datos[]
+     radio.read(datos,sizeof(datos));
+     
+     //reportamos por el puerto serial los datos recibidos
+     Serial.print("Dato0= " );
+     Serial.print(datos[0]);
+     Serial.print(" V, ");
+     Serial.print("Dato1= " );
+     Serial.print(datos[1]);
+     Serial.print(" ms, ");
+     Serial.print("Dato2= " );
+     Serial.println(datos[2]);
+  }
+ else
+ {
+     Serial.println("No hay datos de radio disponibles");
+ }
+ delay(1000);
+}
